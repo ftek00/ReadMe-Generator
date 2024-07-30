1163095f56f2ae10c402fbd92e1f339081e26ac7
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-29 21:10:18 -0400

    created init function

--- index.js
+++ index.js
@@ -35,10 +35,41 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const fs = require("fs");
+
+  fs.writeFile(fileName, data, (err) => {
+    err
+      ? console.error(err)
+      : console.log(`${fileName} has been successfully created.`);
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = `
+  # ${answers.projectTitle}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Installation
+  ${answers.installationInstructions}
+  
+  ## Usage
+  ${answers.usageInformation}
+  
+  ## Contribution Guidelines
+  ${answers.contributionGuidelines}
+  
+  ## Test Instructions
+  ${answers.testInstructions}
+  `;
+
+    writeToFile("README.md", readmeContent);
+  });
+}
 
 // Function call to initialize app
 init();
