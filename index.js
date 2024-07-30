d6d92b2ba88f3f0963723fbbb5ca76bf0f8f072b
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-29 20:50:07 -0400

    included package andcreated array of questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "Enter the title of your project:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description for your project:",
+  },
+  {
+    type: "input",
+    name: "installationInstructions",
+    message: "Enter installation instructions for your project:",
+  },
+  {
+    type: "input",
+    name: "usageInformation",
+    message: "Enter usage information for your project:",
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "Enter contribution guidelines for your project:",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "Enter test instructions for your project:",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
